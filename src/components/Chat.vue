e7a4c5bdf0de7afaaa9b5122231eab4b85c4b657
Bianca Silva <bsilva@voxdata.local>
2019-08-07 17:45:18 -0300

    Update chat layout

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,33 +1,49 @@
 <template>
   <div class="container">
-    <div class="card">
-      <div class="card-title">
-        <h3>Chat Group</h3>
-        <hr />
-      </div>
-      <b-card v-for="msg in messages" :key="msg.message">
-        <p>{{msg.message}}</p>
-        <p>{{msg.user}}</p>
-      </b-card>
+    <div class="text-center">
+      <h3 class="mt-3">Real-time chat application with NodeJs, Socket.io and Vue.js demo</h3>
     </div>
-    <div class="card mt-3 p-3" v-if="activeChat">
+    <div class="row">
+      <div class="col-lg-6">
+        <b-card
+          no-body
+          class="mt-3 pb-0 primary"
+          v-for="msg in messages"
+          :key="msg.message"
+          :header="msg.message"
+        >
+          <b-card-text>
+            <p class="mb-0 pl-3">
+              <em>
+                <small>User: {{msg.user}}</small>
+              </em>
+            </p>
+          </b-card-text>
+        </b-card>
+      </div>
+    </div>
+    <div class="mt-3" v-if="activeChat">
       <form @submit.prevent="sendMessage">
         <div class="row">
-          <div class="form-group col">
-            <label for="message">Message:</label>
-            <input type="text" v-model="message" class="form-control" />
-          </div>
-        </div>
-        <div class="row">
-          <div class="col text-center">
-            <button type="submit" class="btn btn-success">Enviar mensagem</button>
+          <div class="input-group m-3">
+            <input
+              type="text"
+              class="form-control"
+              v-model="message"
+              placeholder="Type your message"
+              aria-label="Recipient's username"
+              aria-describedby="button-addon2"
+            />
+            <div class="input-group-append">
+              <button class="btn btn-primary" type="submit" id="button-addon2">Send</button>
+            </div>
           </div>
         </div>
       </form>
     </div>
-    <div v-else>
+    <div class="card p-5" v-else>
       <div class="form-group col">
-        <label for="user">Nome de usuário:</label>
+        <label for="user">Username</label>
         <input type="text" v-model="user" class="form-control" />
       </div>
       <div class="from-group col">
